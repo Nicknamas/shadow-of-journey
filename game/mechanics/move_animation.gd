94fecf4aa0dc533df3642027b61d64c801d1779e
Alexey <a.kakunin23@gmail.com>
2025-06-21 17:44:36 +0800

    decomposition of script for idle and walk

---
+++ game/mechanics/move_animation.gd
@@ -0,0 +1,39 @@
+class_name MoveAnimation extends Node2D
+
+@export var _animated_sprite: AnimatedSprite2D
+
+enum ANIMATION_STATES { WALK, IDLE }
+enum LAST_DIRECTIONS { LEFT, RIGHT, DOWN, UP }
+
+var _walk_controller: WalkAnimation
+var _idle_controller: IdleAnimation
+
+var last_direction = LAST_DIRECTIONS.RIGHT
+
+func play_animation(animation_type: ANIMATION_STATES, vector_movement: Vector2) -> void:
+	if animation_type == ANIMATION_STATES.WALK:
+		_walk_controller.play_animation_walk(vector_movement)
+		last_direction = _walk_controller.get_last_direction()
+	if animation_type == ANIMATION_STATES.IDLE:
+		_idle_controller.play_animation_idle(last_direction)
+
+func _handle_input() -> void:
+	var vector_movement = Input.get_vector("left", "right", "down", "up")
+	_logging_vector(vector_movement)
+	if vector_movement.x != 0 or vector_movement.y != 0:
+		play_animation(ANIMATION_STATES.WALK, vector_movement)
+	else:
+		play_animation(ANIMATION_STATES.IDLE, vector_movement)
+
+
+func _logging_vector(vector_movement: Vector2) -> void:
+	print(vector_movement)
+
+
+func _ready():
+	_walk_controller = WalkAnimation.new(_animated_sprite)
+	_idle_controller = IdleAnimation.new(_animated_sprite)
+
+
+func _physics_process(delta) -> void:
+	_handle_input()
---
+++ game/mechanics/walk_animation.gd
@@ -0,0 +1,51 @@
+class_name WalkAnimation extends Node
+
+var _animated_sprite: AnimatedSprite2D
+enum DIRECTION { UP = 2, RIGHT = 1, LEFT = -1, DOWN = -2 }
+
+var last_direction: DIRECTION = DIRECTION.RIGHT
+
+const ANIMATION_DIRECTIONS = {
+	2: "UP",
+	1: "RIGHT",
+	-1: "LEFT",
+	-2: "DOWN"
+}
+
+const ANIMATION_NAMES = {
+	ANIMATION_DIRECTIONS[DIRECTION.UP]: "walk_up",
+	ANIMATION_DIRECTIONS[DIRECTION.RIGHT]: "walk_side",
+	ANIMATION_DIRECTIONS[DIRECTION.LEFT]: "walk_side",
+	ANIMATION_DIRECTIONS[DIRECTION.DOWN]: "walk_down"
+}
+
+
+func play_animation_walk(vector_movement: Vector2) -> void:
+	var animation_direction = _get_animation_name_by_vector(vector_movement)
+	last_direction = DIRECTION[animation_direction]
+	if animation_direction == "RIGHT":
+		_animated_sprite.flip_h = true
+	elif animation_direction == "LEFT":
+		_animated_sprite.flip_h = false
+
+	var animation_name = ANIMATION_NAMES[animation_direction]
+	_animated_sprite.play(animation_name)
+
+
+func get_last_direction() -> DIRECTION:
+	return last_direction
+
+
+func _is_right_side_walk(vector_movement: Vector2) -> bool:
+	return vector_movement.x > 0
+
+
+func _get_animation_name_by_vector(vector_movement: Vector2) -> String:
+	var x = vector_movement.x
+	var doubled_y = vector_movement.y * 2
+	var rounded_sum = int(round(x + doubled_y))
+	return ANIMATION_DIRECTIONS.get(rounded_sum)
+
+
+func _init(animated_sprite: AnimatedSprite2D):
+	_animated_sprite = animated_sprite

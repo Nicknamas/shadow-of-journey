e7ad206992b0f0b7af8a0eb63b6fa33c0fa09277
Alexey <a.kakunin23@gmail.com>
2025-06-24 02:43:34 +0800

    change definitions variables

--- game/mechanics/generator.gd
+++ game/mechanics/generator.gd
@@ -1,16 +1,16 @@
 extends Node2D
 
-@export var _dimensions : Vector2i = Vector2(4, 4)
-@export var _start_room : Room
-@export var _critical_path_length : int = 6
-@export var _branches : int = 3
-@export var _branches_length : Vector2i = Vector2i(1, 3)
+@export var dimensions : Vector2i = Vector2(4, 4)
+@export var start_room : Room
+@export var critical_path_length : int = 6
+@export var branches : int = 3
+@export var branches_length : Vector2i = Vector2i(1, 3)
 @export var room_scene : PackedScene
 @export var room_script : Script
 
 
 var room_instance : Room
-var _branch_canditates : Array[Room]
+var branch_canditates : Array[Room]
 var dungeon : Array
 var count_rooms : int
 var tilemap_size : Vector2i
@@ -25,7 +25,7 @@ const ROOM_TYPES = {
 
 
 func _ready() -> void:
-	await self._generate_dungeon()
+	await self.generate_dungeon()
 	self.render_dungeon()
 	self.init_position_hero()
 
@@ -33,9 +33,9 @@ func _ready() -> void:
 func init_position_hero() -> void:
 	var hero = get_tree().get_first_node_in_group("Hero") as Node2D
 	hero.global_position = Vector2(
-		self._start_room.coords.x * self.tilemap_size.x * self.texture_size, 
-		self._start_room.coords.y * self.tilemap_size.y * self.texture_size
-		)
+		self.start_room.coords.x * self.tilemap_size.x * self.texture_size, 
+		self.start_room.coords.y * self.tilemap_size.y * self.texture_size
+	)
 
 
 func render_dungeon() -> void:
@@ -48,59 +48,59 @@ func render_dungeon() -> void:
 				room.global_position = Vector2(room_position_x, room_position_y)
 
 
-func _generate_dungeon() -> void:
+func generate_dungeon() -> void:
 	self.room_instance = self.room_scene.instantiate()
 	await get_parent().ready
 	get_parent().add_child(self.room_instance)
 	if !self.room_instance.is_node_ready():
 		await self.room_instance.ready
 	self.tilemap_size = self.room_instance.size
-	_clear_values()
-	_initializy_dungeons()
-	_place_entrace()
-	_generate_critical_path()
-	_generate_branches()
-	_print_dungeons()
+	clear_values()
+	initializy_dungeons()
+	place_entrace()
+	generate_critical_path()
+	generate_branches()
+	print_dungeons()
 
 
-func _clear_values() -> void:
+func clear_values() -> void:
 	self.dungeon.clear()
-	self._branch_canditates.clear()
-	self._start_room = _create_room(ROOM_TYPES.START)
+	self.branch_canditates.clear()
+	self.start_room = create_room(ROOM_TYPES.START)
 
 
-func _initializy_dungeons() -> void:
-	for x in _dimensions.x:
+func initializy_dungeons() -> void:
+	for x in dimensions.x:
 		dungeon.append([])
-		for y in _dimensions.y:
+		for y in dimensions.y:
 			dungeon[x].append(0)
 
 
-func _place_entrace() -> void:
-	if _start_room.coords.x < 0 or _start_room.coords.x > _dimensions.x:
-		_start_room.coords.x = randi_range(0, _dimensions.x - 1)
-	if _start_room.coords.y < 0 or _start_room.coords.y > _dimensions.y:
-		_start_room.coords.y = randi_range(0, _dimensions.y - 1)
-	dungeon[_start_room.coords.x][_start_room.coords.y] = _start_room
+func place_entrace() -> void:
+	if start_room.coords.x < 0 or start_room.coords.x > dimensions.x:
+		start_room.coords.x = randi_range(0, dimensions.x - 1)
+	if start_room.coords.y < 0 or start_room.coords.y > dimensions.y:
+		start_room.coords.y = randi_range(0, dimensions.y - 1)
+	dungeon[start_room.coords.x][start_room.coords.y] = start_room
 
 
-func _create_room(type_room : String, init_exit : Vector2i = Vector2i(-1, -1)) -> Room:
+func create_room(type_room : String, init_exit : Vector2i = Vector2i(-1, -1)) -> Room:
 	var room: Room = self.room_instance.duplicate()
 	room.set_values(type_room, init_exit)
 	return room
 
 
-func _generate_critical_path() -> void:
-	_generate_path(_start_room, _critical_path_length, ROOM_TYPES.CRITICAL)
+func generate_critical_path() -> void:
+	generate_path(start_room, critical_path_length, ROOM_TYPES.CRITICAL)
 
 
-func _is_includes_coords_in_dungeon(room: Room, direction: Vector2i) -> bool:
-	var includes_x_in_dungeon: bool = room.coords.x + direction.x >= 0 and room.coords.x + direction.x < _dimensions.x
-	var includes_y_in_dungeon: bool = room.coords.y + direction.y >= 0 and room.coords.y + direction.y < _dimensions.y
+func is_includes_coords_in_dungeon(room: Room, direction: Vector2i) -> bool:
+	var includes_x_in_dungeon: bool = room.coords.x + direction.x >= 0 and room.coords.x + direction.x < dimensions.x
+	var includes_y_in_dungeon: bool = room.coords.y + direction.y >= 0 and room.coords.y + direction.y < dimensions.y
 	return includes_x_in_dungeon and includes_y_in_dungeon
 
 
-func _is_critical_path_connect_itself(current_room : Room, direction : Vector2i) -> bool:
+func is_critical_path_connect_itself(current_room : Room, direction : Vector2i) -> bool:
 	if current_room.type_room != ROOM_TYPES.CRITICAL:
 		return false
 
@@ -120,38 +120,38 @@ func _is_critical_path_connect_itself(current_room : Room, direction : Vector2i)
 	return count_connection > 1
 
 
-func _generate_path(from_room : Room, length : int, marker : String) -> bool:
+func generate_path(from_room : Room, length : int, marker : String) -> bool:
 	if length == 0:
 		return true
 	var current_room : Room = from_room
-	var direction : Vector2i = _generate_random_direction()
+	var direction : Vector2i = generate_random_direction()
 	count_rooms += 1
 	
 	for i in 4:
 		if (
-			_is_includes_coords_in_dungeon(current_room, direction) and 
-			not _is_critical_path_connect_itself(current_room, direction) and
+			is_includes_coords_in_dungeon(current_room, direction) and 
+			not is_critical_path_connect_itself(current_room, direction) and
 			not dungeon[current_room.coords.x + direction.x][current_room.coords.y + direction.y] 
 		):
 			var new_coords = current_room.coords + direction
-			var new_room = _create_room(marker, -direction)
+			var new_room = create_room(marker, -direction)
 			new_room.coords = new_coords
 			dungeon[new_room.coords.x][new_room.coords.y] = new_room
 			if marker == ROOM_TYPES.START_BRANCH:
 				marker = ROOM_TYPES.BRANCH
 			if length > 1:
-				_branch_canditates.append(new_room)
-			if _generate_path(new_room, length - 1, marker):
+				branch_canditates.append(new_room)
+			if generate_path(new_room, length - 1, marker):
 				return true
 			else:
-				_branch_canditates.erase(new_room)
+				branch_canditates.erase(new_room)
 				dungeon[new_room.coords.x][new_room.coords.y] = 0
 		direction = Vector2(direction.y, -direction.x)
 	count_rooms -= 1
 	return false
 
 
-func _generate_random_direction() -> Vector2i:
+func generate_random_direction() -> Vector2i:
 	match randi_range(0, 2):
 		0:
 			return Vector2i.UP
@@ -162,21 +162,21 @@ func _generate_random_direction() -> Vector2i:
 	return Vector2i.LEFT
 
 
-func _generate_branches() -> void:
+func generate_branches() -> void:
 	var branches_created : int = 0
 	var candidate : Room
-	while branches_created < _branches and _branch_canditates.size():
-		candidate = _branch_canditates[randi_range(0, _branch_canditates.size() - 1)]
-		if _generate_path(candidate, randi_range(_branches_length.x, _branches_length.y), ROOM_TYPES.START_BRANCH):
+	while branches_created < branches and branch_canditates.size():
+		candidate = branch_canditates[randi_range(0, branch_canditates.size() - 1)]
+		if generate_path(candidate, randi_range(branches_length.x, branches_length.y), ROOM_TYPES.START_BRANCH):
 			branches_created += 1
 		else:
-			_branch_canditates.erase(candidate)
+			branch_canditates.erase(candidate)
 
 
-func _print_dungeons() -> void:
+func print_dungeons() -> void:
 	var dungeon_as_string: String = ""
-	for y in _dimensions.y:
-		for x in _dimensions.x:
+	for y in dimensions.y:
+		for x in dimensions.x:
 			if dungeon[x][y]:
 				dungeon_as_string += "[" + dungeon[x][y].type_room[0] + dungeon[x][y].type_room[1] + "]"
 			else:

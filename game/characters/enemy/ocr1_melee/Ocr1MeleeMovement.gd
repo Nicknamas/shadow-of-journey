bd87a502d73d1ff4966eb4c267ade0ca61cdb79f
KuRaJ <Kuraj.com>
2025-06-27 11:18:32 +0800

    range attack

--- game/characters/enemy/ocr1_melee/Ocr1MeleeMovement.gd
+++ game/characters/enemy/ocr1_melee/Ocr1MeleeMovement.gd
@@ -1,25 +1,24 @@
 extends MovementBase
 
 @export var target: Node2D = null
-var is_player_inside: bool = false
+@export var aggro_radius: float = 100.0
+@export var attack_radius: float = 40.0
 
 func _ready():
-	$AggroArea.body_entered.connect(_on_body_entered)
-	$AggroArea.body_exited.connect(_on_body_exited)
-
-func _on_body_entered(body):
-	if body.name == "Hero":
-		target = body
-		is_player_inside = true
-
-func _on_body_exited(body):
-	if body == target:
-		is_player_inside = false
-		target = null
+	if not target:
+		target = get_tree().get_current_scene().find_child("Hero", true, false)
 
 func _physics_process(delta: float) -> void:
-	if is_player_inside and target and is_instance_valid(target):
-		direction = (target.global_position - global_position).normalized()
+	if target and is_instance_valid(target):
+		var distance_to_player = global_position.distance_to(target.global_position)
+
+		if distance_to_player <= aggro_radius and distance_to_player > attack_radius:
+			direction = (target.global_position - global_position).normalized()
+		else:
+			direction = Vector2.ZERO
+
+		if distance_to_player <= attack_radius:
+			print("Атака игрока!")
 	else:
 		direction = Vector2.ZERO
 

ceca17ac1fa71a78f77f4af494ca20b1b2cc0492
Alexey <a.kakunin23@gmail.com>
2025-06-27 18:12:00 +0800

    add debug for enemy

--- game/characters/enemy/orc_melee/orc_melee_movement.gd
+++ game/characters/enemy/orc_melee/orc_melee_movement.gd
@@ -1,14 +1,16 @@
 extends MovementBase
 
-@export var target: Node2D = null
-@export var aggro_radius: float = 100.0
-@export var attack_radius: float = 20.0
+@export var target : Node2D = null
+@export var aggro_radius : float = 100.0
+@export var attack_radius : float = 20.0
+@export var debug : bool = false
 
 func _ready():
 	if not self.target:
 		self.target = get_tree().get_current_scene().find_child("Hero", true, false)
 
-func _physics_process(delta: float) -> void:
+
+func _physics_process(delta : float) -> void:
 	if self.target and is_instance_valid(target):
 		var distance_to_player = global_position.distance_to(self.target.global_position)
 
@@ -18,7 +20,8 @@ func _physics_process(delta: float) -> void:
 			self.direction = Vector2.ZERO
 
 		if distance_to_player <= attack_radius:
-			print("Атака игрока!")
+			if debug:
+				print("Атака игрока!")
 	else:
 		self.direction = Vector2.ZERO
 
